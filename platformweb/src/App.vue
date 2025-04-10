45a77c8bebd30915f5b85bdacdc9725d296af51a
TlIsD <965229800@qq.com>
2025-04-10 21:31:58 +0800

    feature:集成vue-router

--- platformweb/src/App.vue
+++ platformweb/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>Hello</div>
+  <router-view></router-view>
 </template>
 
 <script setup>
---
+++ platformweb/src/router/index.js
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+// 路由列表
+const routes = [
+
+]
+
+// 实例化
+const router = createRouter({
+    // 指定路由模式
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
