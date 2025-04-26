95ca71084e7d74b51dc330bd12f837b9c0579ca2
TlIsD <965229800@qq.com>
2025-04-26 15:52:12 +0800

    feature:后端添加课程分类api

--- platformapi/platformapi/apps/courses/serializers.py
+++ platformapi/platformapi/apps/courses/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import CourseDirection
+from .models import CourseDirection, CourseCategory
+
 
 class CourseDirectionSerializer(serializers.ModelSerializer):
     # 学习方向序列化器
     class Meta:
             model = CourseDirection
             fields = ['id', 'name']
+
+class CourseCategorySerializer(serializers.ModelSerializer):
+    # 课程分类序列化器
+    class Meta:
+        model = CourseCategory
+        fields = ['id', 'name']
--- platformapi/platformapi/apps/courses/views.py
+++ platformapi/platformapi/apps/courses/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from .models import CourseDirection
-from .serializers import CourseDirectionSerializer
+from .models import CourseDirection, CourseCategory
+from .serializers import CourseDirectionSerializer, CourseCategorySerializer
+
 
 # Create your views here.
 class CourseDirectionListAPIView(ListAPIView):
     # 学习方向
     queryset = CourseDirection.objects.filter(is_show=True, is_deleted=False).order_by('order', '-id')
     serializer_class = CourseDirectionSerializer
+    pagination_class = None
+
+
+class CourseCategoryListAPIView(ListAPIView):
+    # 学习分类
+    queryset = CourseCategory.objects.filter(is_show=True, is_deleted=False).order_by('order', '-id')
+    serializer_class = CourseCategorySerializer
+    pagination_class = None
