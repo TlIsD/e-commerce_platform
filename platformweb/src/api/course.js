d4a0032cbe78ebd1c035c84751f91e4a24491f13
TlIsD <965229800@qq.com>
2025-04-26 22:57:13 +0800

    feature:前端请求学习方向和课程分类数据展示

---
+++ platformweb/src/api/course.js
@@ -0,0 +1,21 @@
+import http from "../utils/http.js";
+import { reactive, ref } from "vue";
+
+const course = reactive({
+    current_direction: 0,  // 当前选中的学习方向
+    direction_list:[],
+    current_category: 0,  // 当前选中的课程分类
+    category_list:[],
+
+    // 获取学习方向信息
+    get_course_direction(){
+        return http.get('/courses/directions/')
+    },
+
+    // 获取课程分类信息
+    get_course_category(){
+        return http.get('/courses/categories/')
+    }
+})
+
+export default course;
