17262c9caf09e517082cc538182f39433e896ef0
TlIsD <965229800@qq.com>
2025-05-04 17:03:10 +0800

    feature:后端提供购物车页面数据

--- platformapi/platformapi/apps/cart/views.py
+++ platformapi/platformapi/apps/cart/views.py
@@ -39,3 +39,36 @@ class CartAPIView(APIView):
 
         # 5. 返回结果给客户端
         return Response({"msg": "成功添加商品课程到购物车！", "cart_total": cart_total}, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        # 获取购物车中商品列表
+        user_id = request.user.id
+        redis = get_redis_connection("cart")
+        cart_hash = redis.hgetall(f"cart_{user_id}")
+        """
+        cart_hash = {
+            b'商品课程ID': b'勾选状态', 
+            b'2': b'1', 
+            b'4': b'1', 
+        }
+        """
+        if len(cart_hash) < 1:
+            return Response({"error": "购物车没有任何商品。"})
+
+        cart = [(int(key.decode()), bool(value.decode())) for key, value in cart_hash.items()]
+        # cart = [ (2,True)， (4,True) ]
+        course_id_list = [item[0] for item in cart]
+        course_list = Course.objects.filter(pk__in=course_id_list, is_deleted=False, is_show=True).all()
+        data = []
+        for course in course_list:
+            data.append({
+                "id": course.id,
+                "name": course.name,
+                "course_cover": course.course_cover.url,
+                "price": float(course.price),
+                "discount": course.discount,
+                "course_type": course.get_course_type_display(),
+                # 勾选状态：把课程ID转换成bytes类型，判断当前ID是否在购物车字典中作为key存在，如果存在，判断当前课程ID对应的值是否是字符串"1"，是则返回True
+                "selected": (str(course.id).encode() in cart_hash) and cart_hash[str(course.id).encode()].decode() == "1"
+            })
+        return Response({"msg": "ok！", "cart": data})
