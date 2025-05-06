f678455fdc0d9355c0e51ae1a17eeb08427a4c33
TlIsD <965229800@qq.com>
2025-05-06 22:24:59 +0800

    feature:后端实现获取用户所有优惠券与本次下单的可用优惠券列表

--- platformapi/platformapi/apps/coupon/urls.py
+++ platformapi/platformapi/apps/coupon/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.CouponListAPIView.as_view()),
+    path("enable/", views.EnableCouponListAPIView.as_view()),
 ]
